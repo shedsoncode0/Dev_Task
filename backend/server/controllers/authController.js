7e81d6ad5617a1be7e191f7ee628a29e2222d4fd
shedrack <aigbeshedrack0@gmail.com>
2023-04-15 11:00:48 +0100

    I added functionalty to the login page and some changes to the backend

--- backend/server/controllers/authController.js
+++ backend/server/controllers/authController.js
@@ -1,7 +1,11 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/UserModel');
-const asyncHandler = require('express-async-handler');
-const { signAccessToken, signRefreshToken, verifyRefreshToken } = require('../../utils/jwt');
+const bcrypt = require("bcryptjs");
+const User = require("../models/UserModel");
+const asyncHandler = require("express-async-handler");
+const {
+  signAccessToken,
+  signRefreshToken,
+  verifyRefreshToken,
+} = require("../../utils/jwt");
 
 /**
  * @description Register User
@@ -9,53 +13,51 @@ const { signAccessToken, signRefreshToken, verifyRefreshToken } = require('../..
  * @access Public
  */
 const register = async (req, res) => {
-    // Get all the user details from the request Body
-    const { fullname, email, password } = req.body;
+  // Get all the user details from the request Body
+  const { fullname, email, password } = req.body;
 
-    if (!fullname || !email || !password) {
-        res.status(400);
-        throw new Error("Please add all fields");
-    }
+  if (!fullname || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
 
-    // Check if Email already exist in the database
-    const emailExit = await User.findOne({ email });
-    if (emailExit) {
-        res.status(400)
-        throw new Error("Email already exist");
-    } 
+  // Check if Email already exist in the database
+  const emailExit = await User.findOne({ email });
+  if (emailExit) {
+    res.status(400);
+    throw new Error("Email already exist");
+  }
 
-    //Hashing the user password
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt);
+  //Hashing the user password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create a new user
-    const user = new User({
-        email,
-        fullname,
-        password: hashedPassword,
+  // Create a new user
+  const user = new User({
+    email,
+    fullname,
+    password: hashedPassword,
+  });
+
+  // Save the user to the database and check for any error
+  try {
+    const savedUser = await user.save();
+
+    // Create a JWT Access Token
+    const accessToken = signAccessToken(savedUser._id);
+    const refreshToken = signRefreshToken(savedUser._id);
+
+    res.status(201).json({
+      // id: savedUser._id,
+      // fullname: savedUser.fullname,
+      // email: savedUser.email,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
     });
-
-    // Save the user to the database and check for any error
-    try {
-        const savedUser = await user.save();
-
-        // Create a JWT Access Token
-        const accessToken = signAccessToken(savedUser._id);
-        const refreshToken = signRefreshToken(savedUser._id);
-
-        res.status(201).json({
-            // id: savedUser._id,
-            // fullname: savedUser.fullname,
-            // email: savedUser.email,
-            accessToken: accessToken, 
-            refreshToken: refreshToken
-        });
-
-    } catch (error) {
-        res.status(500)
-        throw new Error("Invalid user data");
-    }
-}
+  } catch (error) {
+    res.status(500).json("Invalid user data");
+  }
+};
 
 /**
  * @description Login User
@@ -63,59 +65,58 @@ const register = async (req, res) => {
  * @access Public
  */
 const login = async (req, res) => {
-     // Get all the user details from the request Body
-     const { email, password } = req.body; 
- 
-     if (!email || !password) return res.status(400).send("All fields are required");
+  // Get all the user details from the request Body
+  const { email, password } = req.body;
 
-     try {
-         // Check if username exit
-        const user = await User.findOne({ email });
-        
-        if (!user) {
-            res.status(404)
-            throw new Error("User not found")
-        } 
+  if (!email || !password)
+    return res.status(400).send("All fields are required");
 
-        // Checking if the user password is correct
-        const checkPassword = await bcrypt.compare(password, user.password);
-        if (!checkPassword) {
-            res.status(401)
-            throw new Error("Username/password not valid")
-        }
+  try {
+    // Check if username exit
+    const user = await User.findOne({ email });
 
-        // Generating JWT Tokens
-        const accessToken = signAccessToken(user._id);
-        const refreshToken = signRefreshToken(user._id);
+    if (!user) {
+      res.status(404);
+      throw new Error("User not found");
+    }
 
-        res.status(200).json({
-            accessToken: accessToken, 
-            refreshToken: refreshToken
-        });
-     } catch (error) {
-        res.status(500);
-        throw new Error(error.message);
-     };
+    // Checking if the user password is correct
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+      res.status(401);
+      throw new Error("Username/password not valid");
+    }
+
+    // Generating JWT Tokens
+    const accessToken = signAccessToken(user._id);
+    const refreshToken = signRefreshToken(user._id);
+
+    res.status(200).json({
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
 
 const refreshToken = async (req, res, next) => {
-    try {
-        const { refreshToken } = req.body;
+  try {
+    const { refreshToken } = req.body;
 
-        if (!refreshToken) throw res.status(500);
-        const userId = verifyRefreshToken(refreshToken);
+    if (!refreshToken) throw res.status(500);
+    const userId = verifyRefreshToken(refreshToken);
 
-        const accessToken = signAccessToken(userId);
-        const refToken = signRefreshToken(userId);
+    const accessToken = signAccessToken(userId);
+    const refToken = signRefreshToken(userId);
 
-        res.status(200).json({AccessToken: accessToken, RefreshToken: refToken});
-
-    } catch (error) {
-        next(error);
-    }
-}
+    res.status(200).json({ AccessToken: accessToken, RefreshToken: refToken });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    register,
-    login
-}
+  register,
+  login,
+};
